183c84a4264631166aecbf0e7b59b8fb1cf5abf8
Chooongg <chooongg@outlook.com>
2020-08-02 16:27:19 +0800

    添加Activity管理器

--- utils/src/main/java/chooongg/frame/manager/ActivityTaskManager.kt
+++ utils/src/main/java/chooongg/frame/manager/ActivityTaskManager.kt
@@ -4,12 +4,27 @@ import android.app.Activity
 import android.app.Application
 import android.os.Bundle
 import java.util.*
+import kotlin.reflect.KClass
 
 object ActivityTaskManager : Application.ActivityLifecycleCallbacks {
 
     val activityTasks = LinkedList<Activity>()
 
     val activityTop: Activity? get() = activityTasks.last
+
+    fun finishAllActivity() {
+        for (activity in activityTasks.descendingIterator()) {
+            activity.finish()
+        }
+    }
+
+    fun finishOtherActivity(clazz: KClass<out Activity>) {
+        for (activity in activityTasks.descendingIterator()) {
+            if (activity::class != clazz) {
+                activity.finish()
+            }
+        }
+    }
 
     override fun onActivityCreated(activity: Activity, saveInstanceState: Bundle?) {
         activityTasks.addLast(activity)
