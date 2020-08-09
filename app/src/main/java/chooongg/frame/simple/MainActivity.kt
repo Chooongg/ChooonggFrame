7d2e0663592fa6839324d4b96c89e661552dde77
Chooongg <chooongg@outlook.com>
2020-08-09 18:22:54 +0800

    添加RecyclerView Divider

--- app/src/main/java/chooongg/frame/simple/MainActivity.kt
+++ app/src/main/java/chooongg/frame/simple/MainActivity.kt
@@ -1,25 +1,18 @@
 package chooongg.frame.simple
 
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import chooongg.frame.ChooonggFrame
 import chooongg.frame.core.activity.ChooonggActivity
 import chooongg.frame.core.annotation.ContentLayout
 import chooongg.frame.core.annotation.TitleBar
 import chooongg.frame.core.annotation.WindowBackground
 import chooongg.frame.core.loadState.LoadUtils
-import chooongg.frame.core.utils.animatorCornerSize
-import chooongg.frame.http.request.retrofitDefault
-import chooongg.frame.simple.api.APIResponse
-import chooongg.frame.simple.api.TestAPI
 import chooongg.frame.utils.doOnClick
 import chooongg.frame.utils.withMain
 import coil.api.load
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @ContentLayout(R.layout.activity_main)
@@ -41,31 +34,33 @@ class MainActivity : ChooonggActivity() {
                 iv_image.load("https://img-blog.csdnimg.cn/20190821180619507.jpg") {
                     crossfade(true)
                     placeholder(R.drawable.ic_launcher_background)
-                    iv_image.animatorCornerSize(10f)
+                    iv_image.doOnClick {
+
+                    }
                 }
             }
         }
     }
 
     override fun initContent(savedInstanceState: Bundle?) {
-        iv_image.doOnClick {
-            val launchIO = lifecycleScope.launch {
-                retrofitDefault<APIResponse<Any>> {
-                    api = TestAPI.service.sendSms("15533906327", 1)
-                    onStart {
-                        showLoading()
-                    }
-                    onSuccess {
-                        Log.e(ChooonggFrame.TAG, "initConfig: 请求成功")
-                    }
-                    onFailed {
-                        Log.e(ChooonggFrame.TAG, "initConfig: 请求失败")
-                    }
-                    onEnd {
-                        hideLoading()
-                    }
-                }
-            }
-        }
+//        iv_image.doOnClick {
+//            val launchIO = lifecycleScope.launch {
+//                retrofitDefault<APIResponse<Any>> {
+//                    api = TestAPI.service.sendSms("15533906327", 1)
+//                    onStart {
+//                        showLoading()
+//                    }
+//                    onSuccess {
+//                        Log.e(ChooonggFrame.TAG, "initConfig: 请求成功")
+//                    }
+//                    onFailed {
+//                        Log.e(ChooonggFrame.TAG, "initConfig: 请求失败")
+//                    }
+//                    onEnd {
+//                        hideLoading()
+//                    }
+//                }
+//            }
+//        }
     }
 }
