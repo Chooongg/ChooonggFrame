4857bbc629e88d0a2484b049354a5c0192f49593
Chooongg <chooongg@outlook.com>
2020-08-11 18:27:46 +0800

    删除Adapter

--- adapter/src/main/java/chooongg/frame/adapter/animation/BaseAnimator.kt
+++
@@ -1,8 +0,0 @@
-package chooongg.frame.adapter.animation
-
-import android.animation.Animator
-import android.view.View
-
-interface BaseAnimator {
-    fun animators(view: View): Array<Animator>
-}
